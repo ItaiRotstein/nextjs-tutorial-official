e9750067beb52f545837a1a6985638dfbe3d89aa
Itai Rotstein <itairotstein@gmail.com>
2024-04-25 19:28:09 +0300

    Added Customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,23 @@
-import { Metadata } from 'next';
- 
-export const metadata: Metadata = {
-  title: 'Customers',
-};
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import Table from '../../ui/customers/table';
+import Search from '@/app/ui/search';
+import { lusitana } from '@/app/ui/fonts';
 
-export default function Page () {
-    return <p>Customers Page</p>
+export default async function Page({
+  searchParams }: {
+    searchParams: { query: string; };
+  }) {
+    
+  const { query } = searchParams;
+  const customers = await fetchFilteredCustomers(query || '');
+
+  return (
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <Search placeholder="Search customers..." />
+      <Table customers={customers} />
+    </div>
+  );
 }
